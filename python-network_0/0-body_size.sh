53b47a46258fe7f2206b3c42b63b0d4f69b89e76
NAILLAHHHH <n.ineza1@alustudent.com>
2025-03-24 16:40:31 +0200

    Update 0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Sends a request to a URL and displays the size of the response body in bytes
-curl -s "$1" | wc -c
+# Sends a request to a URL and displays the size of the body of the response
+response=$(curl -s "$1")
+echo -n "$response" | wc -c
